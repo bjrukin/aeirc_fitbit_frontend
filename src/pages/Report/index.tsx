eb2e80ed5f2aa85bbea8c73832cff257041ce1c1
Sumit Ghimire <sumitghimire018@gmail.com>
2024-02-28 12:07:11 +0545

    fix:refractoring

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,12 +1,26 @@
 import DashboardLayout from "../../components/ui/DashboardLayout";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card";
+
 
 const Report = () => {
   return (
     <DashboardLayout>
-      <div>Report</div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
     </DashboardLayout>
   );
 };
 
 export default Report;
-    
