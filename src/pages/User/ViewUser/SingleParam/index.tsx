e0b891ce538cd65541df96a06797ac1540405538
Sumit Ghimire <sumitghimire018@gmail.com>
2024-03-21 09:10:36 +0545

    refractor

--- src/pages/User/ViewUser/SingleParam/index.tsx
+++ src/pages/User/ViewUser/SingleParam/index.tsx
@@ -1,9 +1,163 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Drop, Vitals } from "../../../../assets/images";
+import BreadCrumub from "../../../../components/shared/BreadCrum";
+import { Button } from "../../../../components/shared/Button";
+import { SimpleLineChart } from "../../../../components/shared/Chart/linechart";
+import { getTimeInMinutes } from "../../../../components/shared/UserCard";
 import DashboardLayout from "../../../../components/ui/DashboardLayout";
+import {
+  ToggleGroup,
+  ToggleGroupItem,
+} from "../../../../components/ui/toggle-group";
 
 const SingleParamDetail = () => {
-  return <DashboardLayout>
-    asd
-  </DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <div className="flex items-center justify-between">
+        <div>
+          <BreadCrumub title={"User Details"} subTitle={"Data History"} />
+          <div className="flex items-center space-x-3 mt-1">
+            {" "}
+            <div className="text-xl font-semibold">User Vital Metrics</div>
+            <img src={Vitals} className="w-[18px] h-[18px]" alt="vitals" />
+          </div>
+        </div>
+        <div>
+          <Button
+            className="w-fit p-4"
+            variant="secondary"
+            text="Download Report"
+          />
+        </div>
+      </div>
+      <div className="mt-5 bg-white w-fit rounded-lg p-4">
+        <ToggleGroup
+          //   name={field?.name}
+          type="single"
+          className="flex justify-start space-x-4"
+          //   value={field.value}
+          //   onValueChange={(value) => {
+          //     setFieldValue(field.name, value);
+          //     console.log("changed vaue", value);
+          //   }}
+        >
+          <ToggleGroupItem
+            className="border-[1px] border-black text-black px-5 py-1 text-base"
+            value={"daily"}
+          >
+            Daily Report
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            className="border-[1px] border-black text-black px-5 py-1 text-base"
+            value={"weekly"}
+          >
+            Weekly Report
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            className="border-[1px] border-black text-black px-5 py-1 text-base"
+            value={"monthly"}
+          >
+            Monthly Report
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
+      <div className="mt-5">
+        <div className="cursor-pointer p-6   hover:border-[1px] hover:border-primary-500 border-[1px] border-tertiary-750  rounded-lg bg-white ">
+          <div className="flex items-center justify-between">
+            <div className="flex space-x-4 items-center">
+              <img src={Drop} alt="drop" className="w-8 h-8 " />
+              <p className="font-semibold text-tertiary-950 text-base ">
+                User Heart Beat
+              </p>
+            </div>
+            <div className=" flex items-center justify-between rounded-lg bg-primary-150 px-4 py-2 w-[383px]">
+              <div className="flex items-center space-x-4">
+                <img src={Drop} alt="" />
+                <p className="text-base text-black semi-bold">
+                  Your Heart Rate Is Normal{" "}
+                </p>
+              </div>
+              <p className="text-secondary-150 font-semibold">74 BPM</p>
+            </div>
+          </div>
+          <div className="pt-[54px] mb-[26px] flex items-center space-x-20">
+            <div>
+              <p className="font-semibold text-xl text-black"> 12 BPM</p>
+              <p className="font-semibold text-lg text-black">
+                {" "}
+                Average Heart Beat
+              </p>
+              <p className="font-semibold text-lg text-tertiary-950">
+                Measured 5 Min ago
+                {/* Measured {getTimeInMinutes(timestamp)} Hours Ago */}
+              </p>
+            </div>
+            <div>
+              <p className="font-semibold text-xl text-black"> 12 BPM</p>
+              <p className="font-semibold text-lg text-black">
+                {" "}
+                Lowest Heart Beat
+              </p>
+              <p className="font-semibold text-lg text-tertiary-950">
+                Measured 5 Min ago
+                {/* Measured {getTimeInMinutes(timestamp)} Hours Ago */}
+              </p>
+            </div>
+            <div>
+              <p className="font-semibold text-xl text-black"> 12 BPM</p>
+              <p className="font-semibold text-lg text-black">
+                {" "}
+                Highest Heart Beat
+              </p>
+              <p className="font-semibold text-lg text-tertiary-950">
+                Measured 5 Min ago
+                {/* Measured {getTimeInMinutes(timestamp)} Hours Ago */}
+              </p>
+            </div>
+          </div>
+          <div className="mt-3">
+            <SimpleLineChart variant={"primary"} />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex w-full  space-x-8 mt-4">
+        <div className="p-4 bg-white w-1/2 border-[1px] border-tertiary-750  rounded-lg bg-white">
+          sd
+        </div>
+        <div className="p-4 bg-white w-1/2 border-[1px] border-tertiary-750  rounded-lg bg-white">
+          <div className="flex space-x-4 items-center mb-5">
+            <img src={Drop} alt="" />
+            <p className="text-black text-base font-semibold">Other Options</p>
+          </div>
+          <div className="rounded-lg bg-[#F4F4F4] p-4 mb-4">
+            {" "}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <img src={Drop} alt="" />
+                <p className="text-black text-base font-semibold">
+                  Health Setting
+                </p>
+              </div>
+              <ChevronRight />
+            </div>
+          </div>
+          <div className="rounded-lg bg-[#F4F4F4] p-4 ">
+            {" "}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <img src={Drop} alt="" />
+                <p className="text-black text-base font-semibold">
+                  Knowledge Data Column
+                </p>
+              </div>
+              <ChevronRight />
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default SingleParamDetail;
--- src/pages/User/ViewUser/index.tsx
+++ src/pages/User/ViewUser/index.tsx
@@ -13,6 +13,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserParam } from "../../../redux/slice/userParamDetail/userParamAction";
 import Service from "../../../setup/Service";
 import { HeartBeatCard } from "../../../components/shared/UserCard/heartBeatCard";
+import { DashboardSkeleton } from "../../../components/shared/skeleton/dashboardSkeleton";
+import NoData from "../../../components/ui/no-result";
 
 const ViewUser = () => {
   const location = useLocation();
@@ -20,7 +22,7 @@ const ViewUser = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
-  const { data, fetchData } = useFetch(`/device/data/${id}`);
+  const { data, fetchData, loading } = useFetch(`/device/data/${id}`);
   const [deviceData, setDeviceData] = useState<any>(null);
   const [historyData, setHistoryData] = useState<any>(null);
   useEffect(() => {
@@ -123,90 +125,120 @@ const ViewUser = () => {
   console.log("The device data is", deviceData);
   return (
     <DashboardLayout>
-      <div className="flex items-center justify-between">
-        <div>
-          <BreadCrumub title={"User"} subTitle={"User Profile"} />
-          <div className="flex items-center space-x-3 mt-1">
-            {" "}
-            <div className="text-xl font-semibold">User Vital Metrics</div>
-            <img src={Vitals} className="w-[18px] h-[18px]" alt="vitals" />
-          </div>
-        </div>
-        <div>
-          <Button
-            className="w-fit p-4"
-            variant="secondary"
-            text="Download Report"
-          />
-        </div>
-      </div>
+      {loading ? (
+        <DashboardSkeleton />
+      ) : (
+        <>
+          {deviceData?.length > 0 ? (
+            <div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <BreadCrumub title={"User"} subTitle={"User Profile"} />
+                    <div className="flex items-center space-x-3 mt-1">
+                      {" "}
+                      <div className="text-xl font-semibold">
+                        User Vital Metrics
+                      </div>
+                      <img
+                        src={Vitals}
+                        className="w-[18px] h-[18px]"
+                        alt="vitals"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <Button
+                      className="w-fit p-4"
+                      variant="secondary"
+                      text="Download Report"
+                    />
+                  </div>
+                </div>
 
-      <div className="mt-7 flex justify-between space-x-4 ">
-        <div className="w-2/3">{renderUserCard("respiratory_rate_value")}</div>
-        <div className="w-1/3 bg-[white] border-[1px] border-tertiary-750  rounded-lg px-4 py-6">
-          <div className=" flex space-x-3 items-center text-base text-tertiary-950 font-semibold">
-            <img src={Drop} alt="drop" />
-            <p>Cholesterol Levels</p>
-          </div>
-          <div className="mt-[30px] flex flex-col space-y-2">
-            {renderUserCard("triglyceride_cholesterol_integer")}
-            {renderUserCard("low_lipoprotein_cholesterol_integer")}
-            {renderUserCard("high_lipoprotein_cholesterol_integer")}
-          </div>
-        </div>
-      </div>
-      <div className="mt-3 flex space-x-4">
-        <div className="flex flex-col space-y-3">
-          <div className="flex space-x-4">
-            {renderUserCard("temperature_value")}
-            {renderUserCard("blood_glucose_value")}
-          </div>
-          {renderUserCard("step_value")}
-          <div className="flex space-x-4">
-            {renderUserCard("blood_ketone_model")}
-            {renderUserCard("blood_sugar_model")}
-          </div>
-          {renderUserCard("uric_acid")}
-        </div>
-        <div className="flex-1">
-          <div className=" p-6  border-[1px] border-tertiary-750 bg-white  rounded-lg w-full">
-            <div className="flex space-x-4 items-center">
-              <img src={Drop} alt="drop" className="w-8 h-8 " />
-              <p className="font-semibold text-tertiary-950 text-base ">
-                Total Sleep Data
-              </p>
+                <div className="mt-7 flex justify-between space-x-4 ">
+                  <div className="w-2/3">
+                    {renderUserCard("respiratory_rate_value")}
+                  </div>
+                  <div className="w-1/3 bg-[white] border-[1px] border-tertiary-750  rounded-lg px-4 py-6">
+                    <div className=" flex space-x-3 items-center text-base text-tertiary-950 font-semibold">
+                      <img src={Drop} alt="drop" />
+                      <p>Cholesterol Levels</p>
+                    </div>
+                    <div className="mt-[30px] flex flex-col space-y-2">
+                      {renderUserCard("triglyceride_cholesterol_integer")}
+                      {renderUserCard("low_lipoprotein_cholesterol_integer")}
+                      {renderUserCard("high_lipoprotein_cholesterol_integer")}
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-3 flex space-x-4">
+                  <div className="flex flex-col space-y-3">
+                    <div className="flex space-x-4">
+                      {renderUserCard("temperature_value")}
+                      {renderUserCard("blood_glucose_value")}
+                    </div>
+                    {renderUserCard("step_value")}
+                    <div className="flex space-x-4">
+                      {renderUserCard("blood_ketone_model")}
+                      {renderUserCard("blood_sugar_model")}
+                    </div>
+                    {renderUserCard("uric_acid")}
+                  </div>
+                  <div className="flex-1">
+                    <div className=" p-6  border-[1px] border-tertiary-750 bg-white  rounded-lg w-full">
+                      <div className="flex space-x-4 items-center">
+                        <img src={Drop} alt="drop" className="w-8 h-8 " />
+                        <p className="font-semibold text-tertiary-950 text-base ">
+                          Total Sleep Data
+                        </p>
+                      </div>
+                      <div className="pt-[35px]">
+                        <p className="font-semibold text-xl text-black">
+                          8 Hours 12 min
+                        </p>
+                        <p className="font-bold text-lg text-text-green">
+                          Normal Sleep
+                        </p>
+                        <p className="font-semibold text-lg text-tertiary-950">
+                          Measured 2 Hour Earlier
+                        </p>
+                      </div>
+                      <div className="mt-3">
+                        <SimpleLineChart variant={"secondary"} />
+                      </div>
+                    </div>
+                    <div className="mt-4 p-6  border-[1px] border-tertiary-750 bg-white  rounded-lg w-full">
+                      <div className="flex space-x-4 items-center">
+                        <img src={Drop} alt="drop" className="w-8 h-8 " />
+                        <p className="font-semibold text-tertiary-950 text-base ">
+                          Blood Oxygen{" "}
+                        </p>
+                      </div>
+                      <div className="pt-[35px]">
+                        <p className="font-semibold text-xl text-black">
+                          90 KPa
+                        </p>
+                        <p className="font-bold text-lg text-text-green">
+                          Normal
+                        </p>
+                        <p className="font-semibold text-lg text-tertiary-950">
+                          Measured 5 Minute Ago
+                        </p>
+                      </div>
+                      <div className="mt-3">
+                        <SimpleLineChart variant={"secondary"} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="pt-[35px]">
-              <p className="font-semibold text-xl text-black">8 Hours 12 min</p>
-              <p className="font-bold text-lg text-text-green">Normal Sleep</p>
-              <p className="font-semibold text-lg text-tertiary-950">
-                Measured 2 Hour Earlier
-              </p>
-            </div>
-            <div className="mt-3">
-              <SimpleLineChart variant={"secondary"} />
-            </div>
-          </div>
-          <div className="mt-4 p-6  border-[1px] border-tertiary-750 bg-white  rounded-lg w-full">
-            <div className="flex space-x-4 items-center">
-              <img src={Drop} alt="drop" className="w-8 h-8 " />
-              <p className="font-semibold text-tertiary-950 text-base ">
-                Blood Oxygen{" "}
-              </p>
-            </div>
-            <div className="pt-[35px]">
-              <p className="font-semibold text-xl text-black">90 KPa</p>
-              <p className="font-bold text-lg text-text-green">Normal</p>
-              <p className="font-semibold text-lg text-tertiary-950">
-                Measured 5 Minute Ago
-              </p>
-            </div>
-            <div className="mt-3">
-              <SimpleLineChart variant={"secondary"} />
-            </div>
-          </div>
-        </div>
-      </div>
+          ) : (
+            <NoData />
+          )}
+        </>
+      )}
     </DashboardLayout>
   );
 };
